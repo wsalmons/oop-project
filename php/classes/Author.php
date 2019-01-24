f25c2befd02df942ffcd70f3ca1e4549d6d45e14
Wyatt Salmons <wyattsalmons@gmail.com>
2019-01-24 15:32:05 -0700

    added mutator and accessors

--- php/classes/Author.php
+++ php/classes/Author.php
@@ -46,7 +46,7 @@ class Author {
 	 * @param int $newAuthorId new value of author id
 	 * @throws UnexpectedValueException if $newAuthorId is not an integer
 	 */
-	public function setProfileId ($newAuthorId) {
+	public function setAuthorId($newAuthorId) {
 		// verify the author id is valid
 		$newAuthorId = filter_var($newAuthorId, FILTER_VALIDATE_INT);
 		if($newAuthorId === false) {
@@ -55,6 +55,72 @@ class Author {
 
 		// convert and store the author id
 		$this->authorId = intval($newAuthorId);
+	}
+
+	/**
+	 *accessor method for author avatar URL
+	 *
+	 * @return int value of profile id
+	 */
+	public function getAuthorAvatarUrl() {
+		return($this->authorAvatarUrl);
+	}
+
+	/**
+	 * mutator method for author avatar URL
+	 *
+	 * @param string $newAuthorAvatarUrl value of author avatar url
+	 * @throws UnexpectedValueException if $newAuthorAvatarUrl is not valid
+	 */
+
+	public function setAuthorAvatarUrl($newAuthorAvatarUrl) {
+		// verify author avatar url is valid
+		$newAuthorAvatarUrl = filter_var($newAuthorAvatarUrl, FILTER_SANITIZE_STRING);
+		if($newAuthorAvatarUrl === false) {
+			throw(new UnexpectedValueException("author avatar url is not a valid string"));
+		}
+	}
+
+	/**
+	 * accessor method for author activation token
+	 */
+	public function getAuthorActivationToken() {
+		return($this->authorActivationToken);
+	}
+
+	/**
+	 * mutator method for author activation token
+	 *
+	 * @param string $newAuthorActivationToken value of author activation token
+	 * @throws UnexpectedValueException if $newAuthorActivationToken is not valid
+	 */
+	public function setAuthorActivationToken($newAuthorActivationToken) {
+		// verify Author Activation Token is valid
+		$newAuthorActivationToken = filter_var($newAuthorActivationToken, FILTER_SANITIZE_STRING);
+		if($newAuthorActivationToken === false) {
+			throw(new UnexpectedValueException("author activation token is not a valid string"));
+		}
+	}
+	/**
+	 * accessor method for author email
+	 */
+	public function getAuthorEmail () {
+		return($this->authorEmail);
+	}
+
+	/**
+	 * mutator method for author email
+	 *
+	 * @param string $newAuthorEmail value of author email
+	 * @throws UnexpectedValueException if $newAuthorEmail is not valid
+	 */
+
+	public function setAuthorEmail($newAuthorEmail) {
+		//verify Author Email is valid
+		$newAuthorEmail = filter_var($newAuthorEmail, FILTER_SANITIZE_STRING);
+		if($newAuthorEmail === false) {
+			throw(new UnexpectedValueException("author email is not a valid string"));
+		}
 	}
 }
 ?>
