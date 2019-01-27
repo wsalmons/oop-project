79d169dd14a91025ba33dde75e2a20a46678da3c
Wyatt Salmons <wyattsalmons@gmail.com>
2019-01-27 01:04:05 -0700

    input to author.php, and edit binary size of authorId

---
+++ php/lib/author.php
@@ -0,0 +1,10 @@
+<?php
+// load the profile class
+
+require_once("Author.php");
+
+//use the constructor to create a new author
+$authorId = new \Deepdivedylan\DataDesign\author("e462fab1-e72d-4730-b9d8-3f7b43bd5ae7",
+"nananananananananananananananana", "author.com", "email@email.com", "rararararararararararararararara", "author");
+
+?>
