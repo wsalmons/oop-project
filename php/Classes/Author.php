409b728064829d58aceb73ec7dccdba88c620c67
Wyatt Salmons <wyattsalmons@gmail.com>
2019-01-27 23:36:10 -0700

    addded require once

--- php/Classes/Author.php
+++ php/Classes/Author.php
@@ -3,6 +3,7 @@ namespace Deepdivedylan\DataDesign;
 
 require_once("autoload.php");
 require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
+require_once("ValidateUuid.php");
 
 use Ramsey\Uuid\Uuid;
 /**
